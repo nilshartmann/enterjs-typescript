85383ab31b5977a5c167cb86011f08a7e1bb8e7d
Nils Hartmann <nils@nilshartmann.net>
2022-06-23 11:56:03 +0200

    fergi

--- src/10_actions.ts
+++ src/10_actions.ts
@@ -1,7 +1,13 @@
-// type PayloadAction<PL, AN = string> = {
-//   actionName: AN,
-//   payload: PL
-// }
+function getLength<T extends string | null>(
+  s: T
+): T extends string ? number : null {
+  // @ts-ignore
+  return 0;
+}
+
+const x = getLength("fasdfasdf");
+const y = getLength(null);
+
 type IncrementAction = {
   incrementBy: number;
 };
@@ -14,6 +20,32 @@ const reducers = {
   increment(state: any, action: IncrementAction) {},
   multiply(state: any, action: MultiplyAction) {},
 };
+
+type Reducers = typeof reducers;
+
+type MakeActionCreator<T extends Function> = T extends (
+  s: any,
+  action: infer ActionType
+) => any
+  ? (action: ActionType) => ActionType
+  : never;
+
+type IncrementActionCreator = MakeActionCreator<typeof reducers.increment>;
+
+type MakeActionCreators<T extends object> = {
+  [K in keyof T]: T[K] extends Function ? MakeActionCreator<T[K]> : never;
+};
+
+function createActionCreators<R extends object>(
+  reducerMap: R
+): MakeActionCreators<R> {
+  // @ts-ignore
+  return "";
+}
+
+const result = createActionCreators(reducers);
+
+type ActionCreators = MakeActionCreators<Reducers>;
 
 // Aufgabe: ich will ein Objekt, dass aus Funktionen besteht,
 // die genauso heißen, wie in reducers,
